e4174f80762b5bbd53ad9dd722c66b70adfeacc3
MarcoMechelli <33416337+MarcoMechelli@users.noreply.github.com>
2017-11-06 11:56:47 +0100

    Create stampa_variabili.c

---
+++ stampa_variabili.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+char prima='e';
+int seconda=12,terza=15;
+float quarta=12.6;
+printf("La prima variabile è uguale ad %c\n",prima);
+printf("La seconda variabile è uguale a %d\n",seconda);
+printf("La terza variabile è uguale a %d\n",terza);
+printf("La quarta variabile è uguale a %f\n",quarta);
+return 0;
+}
